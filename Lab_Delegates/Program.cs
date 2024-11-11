cef20d2694b220dd3d817b006836d01c739447b3
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-11-11 14:04:43 +0100

    Lab 003 Vorgabe

---
+++ Lab_Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab_Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\nDelegaten");
+            Labs.Delegates.Run(args);
+
+            Console.WriteLine("\n\nPrimzahlen");
+            Labs.PrimeMain.Run(args);
+        }
+    }
+}
