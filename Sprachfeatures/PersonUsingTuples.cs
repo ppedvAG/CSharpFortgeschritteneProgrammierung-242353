2a6a33cf82e2c86d713783e815c3d0c6c70d9ec1
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-11-11 12:42:17 +0100

    Module 001 Sprachfeatures C# 7 bis C# 9

---
+++ Sprachfeatures/PersonUsingTuples.cs
@@ -0,0 +1,32 @@
+﻿namespace Sprachfeatures
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Diverse
+    }
+
+    public class PersonUsingTuples
+    {
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Gender Gender { get; set; } = Gender.Diverse;
+
+        public (string FirstName, string MiddleName, string LastName, Gender Gender) GetFullName()
+        {
+            return (FirstName, !string.IsNullOrWhiteSpace(MiddleName) ? MiddleName : string.Empty, LastName, Gender);
+        }
+
+        // Vor C# 7
+        public Tuple<string, string, string, Gender> GetFullNameOld()
+        {
+            var middleName = !string.IsNullOrWhiteSpace(MiddleName) ? MiddleName : string.Empty;
+            return Tuple.Create(FirstName, middleName, LastName, Gender);
+        }
+    }
+}
---
+++ Sprachfeatures/Program.cs
@@ -0,0 +1,114 @@
+﻿
+
+
+namespace Sprachfeatures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tuple Samples");
+            TupleSamples();
+
+            Console.WriteLine("\nLocal Functions Sample");
+            LocalFunctionSample();
+
+            Console.WriteLine("\nYield Sample");
+            YieldSample();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void TupleSamples()
+        {
+            var hugo = new PersonUsingTuples
+            {
+                FirstName = "Hugo",
+                LastName = "Boss",
+                MiddleName = "Von"
+            };
+
+            // Alter Ansatz
+            var tuples = hugo.GetFullNameOld();
+            Console.WriteLine("Full Name old");
+            Console.WriteLine(string.Format("Name: {0} {1} {2}", tuples.Item1, tuples.Item2, tuples.Item3));
+
+            // Neuer Ansatz welcher deutlich lesbarer und dadurch besser ist
+            // Im Hintergrund aber uebersetzt der Compiler das in das alte Pattern
+            var namedTuples = hugo.GetFullName();
+            Console.WriteLine("Full Name with named tuples");
+            Console.WriteLine($"Name: {namedTuples.FirstName} {namedTuples.MiddleName} {namedTuples.LastName}");
+
+            PersonUsingTuples karla = new()
+            {
+                FirstName = "Karla",
+                MiddleName = "Viola",
+                LastName = "Friedrich"
+            };
+
+            // Dekonstruierende Zuweisung
+            // _ verwenden um ihn als "discarded" zu kennzeichnen (benoetigt noch nicht mal mehr "var")
+            var (firstname, middlename, lastname, _) = karla.GetFullNameOld();
+            Console.WriteLine(firstname);
+            Console.WriteLine(middlename);
+            Console.WriteLine(lastname);
+        }
+
+
+        private static void LocalFunctionSample()
+        {
+            DoubleValue(5_345);
+
+            var result = Fibonacci(10);
+            Console.WriteLine($"10. Fibonacci-Zahl: {result}");
+        }
+
+        private static void DoubleValue(int value = 123)
+        {
+            Action doubleValue = () => value *= 2;
+            doubleValue();
+            Console.WriteLine(value);
+
+            // locale function als Alternative
+            // Nuetzlich wenn man mehr als nur ein Einzeiler hat
+            void localFunc()
+            {
+                value *= 2;
+            }
+            localFunc();
+            Console.WriteLine(value);
+        }
+
+        public static int Fibonacci(int start)
+        {
+            return recursiveCalc(start).currentValue;
+
+            static (int currentValue, int previousValue) recursiveCalc(int start)
+            {
+                if (start == 0) return (1, 0);
+                var (current, previous) = recursiveCalc(start - 1);
+                return (current + previous, current);
+            }
+        }
+
+        private static void YieldSample()
+        {
+            IEnumerable<PersonUsingTuples> students = GetStudents();
+            Console.WriteLine("Students wurde noch nicht evaluiert.");
+
+            List<PersonUsingTuples> studentList = students.ToList();
+            Console.WriteLine("Students wurde evaluiert.");
+
+            studentList.ForEach(s => Console.WriteLine(s.FirstName));
+
+            // Ein IEnumerable wird nicht sofort aufgeloest sondern erst beim ersten Zugriff auf die Daten
+            // vgl. Builder-Pattern
+            IEnumerable<PersonUsingTuples> GetStudents()
+            {
+                yield return new PersonUsingTuples { FirstName = "Hugo", LastName = "Boss", MiddleName = "Von" };
+                yield return new PersonUsingTuples { FirstName = "Karla", LastName = "Friedrich", MiddleName = "Viola" };
+            }
+        }
+    }
+}
