20cc326f75f0785249d43d3835a05daf470d70ae
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-11-12 11:29:58 +0100

    Lab 006 Kontoverwaltung

---
+++ Lab_Konto/KontoWithMutex.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_Konto
+{
+    public class KontoWithMutex : IKonto
+    {
+        public string Type { get; init; }
+        public int Balance { get; set; } = 0;
+        public int TransactionCount { get; set; } = 0;
+
+        private readonly Mutex _mutex = new Mutex();
+
+        public void Deposite(int value)
+        {
+            _mutex.WaitOne();
+            Balance += value;
+            TransactionCount++;
+            Console.WriteLine($"Kontostand ({Type}): {Balance}");
+            _mutex.ReleaseMutex();
+        }
+
+        public void Disburse(int value)
+        {
+            Deposite(-value);
+        }
+    }
+}
---
+++ Lab_Konto/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Lab_Konto
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateThreads<KontoWithLock>(15);
+
+            CreateThreads<KontoWithMutex>(15);
+
+            Console.WriteLine("Fertig");
+            Console.Read();
+        }
+
+        private static void CreateThreads<T>(int count) where T : IKonto, new()
+        {
+            var konto = new T() { Type = typeof(T).Name };
+            for (int i = 0; i < count; i++)
+            {
+                new Thread(() => Run(konto)).Start();
+            }
+        }
+
+        static void Run(object arg) //Random Einzahlungen und Auszahlungen ausführen
+        {
+            var konto = (IKonto)arg;
+            for (int i = 0; i < 10; i++)
+            {
+                int betrag = Random.Shared.Next(0, 10) * 10;
+
+                if (Random.Shared.Next() % 2 == 0)
+                    konto.Deposite(betrag);
+                else
+                    konto.Disburse(betrag);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
