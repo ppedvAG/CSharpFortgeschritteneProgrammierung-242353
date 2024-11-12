434042d2c1c99a384dd92e9087ddde8da5eccfb8
Ortwin Carstocea <OrtwinC@ppedv.de>
2024-11-12 13:46:45 +0100

    Renamed folders

---
+++ TPL_Parallel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TPL_Parallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
